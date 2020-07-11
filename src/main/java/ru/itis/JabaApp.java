73926242854cfabc89a392c82b1434198788d15e
Bogacheva Viktoria <viktori.serr@gmail.com>
2020-07-11 22:17:09 +0300

    jaba init

---
+++ src/main/java/ru/itis/JabaApp.java
@@ -0,0 +1,14 @@
+package ru.itis;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JabaApp.class, args);
+    }
+    
+}
